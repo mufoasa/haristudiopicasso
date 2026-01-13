4cc4e5b82326ef9907d21a30fefad0f5ccddaa20
mufoasa <96444527+mufoasa@users.noreply.github.com>
2026-01-13 02:03:59 +0000

    Update whatsapp.js

--- netlify/functions/whatsapp.js
+++ netlify/functions/whatsapp.js
@@ -1,19 +1,14 @@
 exports.handler = async (event) => {
-  const params = new URLSearchParams(event.body || "");
-  const message = params.get("Body");
-
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/xml" },
     body: `
-      <Response>
-        <Message>
+<Response>
+  <Message>
 👋 HairStudio Picasso
-
 WhatsApp rezervimi është aktiv ✅
-Shkruani "rezervim" për të vazhduar.
-        </Message>
-      </Response>
+  </Message>
+</Response>
     `
   };
 };
